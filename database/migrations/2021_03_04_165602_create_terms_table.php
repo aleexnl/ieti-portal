134bfef1da00031785a82189684ba93b1b5d31b8
aleexnl <alex.nieto0027@gmail.com>
2021-03-04 18:09:54 +0100

    add: terms table migration

---
+++ database/migrations/2021_03_04_165602_create_terms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->text('description');
+            $table->date('start_date');
+            $table->text('end_date');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('terms');
+    }
+}
