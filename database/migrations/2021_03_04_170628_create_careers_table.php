58f377c6d19b28c48b53b6dca0171f347f528a88
aleexnl <alex.nieto0027@gmail.com>
2021-03-04 18:35:47 +0100

    add: created caeers table migration

---
+++ database/migrations/2021_03_04_170628_create_careers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCareersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code');
+            $table->text('description');
+            $table->foreign('term_id')->references('id')->on('terms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('careers');
+    }
+}
