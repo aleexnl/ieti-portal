df10ba3edad330a5cff456eb63b5a6ae3e04c714
aleexnl <alex.nieto0027@gmail.com>
2021-03-04 18:37:32 +0100

    add: created mps table migration

---
+++ database/migrations/2021_03_04_170652_create_mps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMpsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mps', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('career_id')->references('id')->on('careers');
+            $table->string('name');
+            $table->string('code');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mps');
+    }
+}
