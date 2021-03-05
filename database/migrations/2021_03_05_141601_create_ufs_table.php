f6f849a2daaadb6aba0aaec69739014a2178d4c2
SergioSoffiati <sergiogarciasoffiati@gmail.com>
2021-03-05 15:53:01 +0100

    Migrations and  landing page

---
+++ database/migrations/2021_03_05_141601_create_ufs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUfsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ufs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('mp_id')->constrained('mps');
+            $table->string('name');
+            $table->string('code');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ufs');
+    }
+}
