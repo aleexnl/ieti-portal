fd734d01891a6349107a967041e169a7a5f48e36
aleexnl <alex.nieto0027@gmail.com>
2021-03-08 12:47:14 +0100

    add: sample page foor look & feel

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/sample-page', function () {
+    return view('sample');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
