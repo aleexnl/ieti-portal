482d8b225b89f03519c660c2bb19b115491369b8
aleexnl <alex.nieto0027@gmail.com>
2021-03-08 13:19:40 +0100

    refactor: updated users factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class UserFactory extends Factory
 {
@@ -22,9 +23,10 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $array = ["admin", "user"];
         return [
             'name' => $this->faker->name,
-            'role' => 'admin',
+            'role' => Arr::random($array),
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
