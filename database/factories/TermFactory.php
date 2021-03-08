8f505f5a411c9e0487cf364141df47f000c0f44a
aleexnl <alex.nieto0027@gmail.com>
2021-03-08 16:49:08 +0100

    add: term factory and seeder

---
+++ database/factories/TermFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Term;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TermFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Term::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->paragraph,
+            'start_date' => now(),
+            'end_date' => now(),
+        ];
+    }
+}
