3c7317f23be0f749bbe96e0988c8a48c3878b328
aexgomezdev <byrambo166@gmail.com>
2021-03-09 13:44:45 +0100

    Normal user control panel finsihed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,22 +13,29 @@
 
     <body class="h-screen overflow-hidden flex items-center justify-center" style="background: #edf2f7;">
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
-        <div class="md:flex flex-col md:flex-row md:min-h-screen w-full ">
-            <div @click.away="open = false" class="flex flex-col w-full md:w-64 text-gray-500 text-lg bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0 bg-blue-900" x-data="{ open: false }">
-                <div class="bg-blue-600	opacity-50 ">
-                    <div class="text-blue-50">
+        <div class="md:flex md:min-h-screen w-full ">
+            <div @click.away="open = false" class="flex flex-col w-full md:w-64 text-gray-500 text-lg bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 flex-shrink-0 bg-blue-800" x-data="{ open: false }">
+                <div class="w-full text-center flex justify-center mt-11 mb-8">
+                    <img class="h-32 w-32" src="https://secure.gravatar.com/avatar/17364d3da6022fc9311dfb3221404a90?s=256&d=mm&r=g">
+                </div>
+                <div class="bg-blue-700	 ">
+                    <div class="text-opacity-100" style="color: #25AAE2">
                         <p class="ml-10 my-4 " href="#">Panel de control </a>
                     </div>
                 </div>
-                <nav :class="{'block': open, 'hidden': !open}" class="flex-grow md:block px-4 pb-4 md:pb-0 md:overflow-y-auto">
-                    <div class="flex flex-col w-full items-center ">
-                        <a class="mt-4" href="#">Cursos </a>
+                <nav :class="{'block': open, 'hidden': !open}" class="flex-grow md:block pb-4 md:pb-0 md:overflow-y-auto">
+                    <div class="flex flex-col w-full">
+                        <a class="ml-10 my-4" href="#">Cursos </a>
 
-                        <a class="mt-4" href="#">Blog</a>
+                        <a class="ml-10 my-4" href="#">Alumnes</a>
                     </div>
                 </nav>
             </div>
+            <div style="font-size: 32px">
+                <p class="text-gray-700 ml-4 my-6">Control panel</p>
+            </div>
         </div>
+
     </body>
 
     </html>
