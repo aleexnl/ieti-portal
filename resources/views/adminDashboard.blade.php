19675feb07c92dfa583f57e7b9ee40aab464e39e
aleexnl <alex.nieto0027@gmail.com>
2021-03-10 11:00:40 +0100

    add: varios options in dashboard

--- resources/views/adminDashboard.blade.php
+++ resources/views/adminDashboard.blade.php
@@ -2,4 +2,16 @@
     <x-slot name="header">
         Dashboard
     </x-slot>
+    <div class="flex">
+        <div class="bg-gray-100 m-5 px-5 rounded-md">
+            <a href="cursos">
+                <h3>Cursos</h3>
+            </a>
+        </div>
+        <div class="bg-gray-100 m-5 px-5 rounded-md">
+            <a href="alumnes">
+                <h3>Alumnes</h3>
+            </a>
+        </div>
+    </div>
 </x-admin-layout>
