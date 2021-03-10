ace7cfe3c2e85048ff044c6c0719117a5d3facef
aleexnl <alex.nieto0027@gmail.com>
2021-03-10 19:53:14 +0100

    refactor: deleted delete button

--- public/js/app.js
+++ public/js/app.js
@@ -41,12 +41,6 @@ $(function () {
         $(this).toggleClass("rotate-180");
         $(this).closest("tr").find(".controls").toggleClass("hidden");
       });
-      $(term).find(".delete-button").on("click", function (event) {
-        var id = event.target.attributes.value.value;
-        var name = $(event.target).closest("tr").find("td > div > span").text();
-        var parent = $(event.target).closest("tr");
-        deleteTerm(id, name, parent);
-      });
       $(term).find(".edit-button").on("click", function () {
         $($(this)).css("display", "none");
         $(term).find(".delete-button").css("display", "none");
@@ -124,31 +118,6 @@ $(function () {
     });*/
   });
 });
-
-function deleteTerm(id, name, parent) {
-  // Reset Modal
-  $("#confirm-delete button.delete-button").attr("disabled", true);
-  $("#confirm-delete input").val("");
-  $("#confirm-delete p  b").text(name);
-  $("#confirm-delete").toggleClass("hidden"); // Activate modal
-  // Compare when changing input
-
-  $("#confirm-delete input").on("input", function () {
-    $(this).val() == name ? $("#confirm-delete button.delete-button").attr("disabled", false) : $("#confirm-delete button.delete-button").attr("disabled", true);
-  });
-  $("#confirm-delete button.delete-button").one("click", function () {
-    $.ajax({
-      headers: {
-        "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content")
-      },
-      url: "/cursos/".concat(id),
-      method: "DELETE"
-    }).done(function () {
-      $(parent).fadeOut();
-      $("#confirm-delete").toggleClass("hidden");
-    });
-  });
-}
 
 /***/ }),
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -25,17 +25,6 @@ $(function () {
                 $(this).closest("tr").find(".controls").toggleClass("hidden");
             });
         $(term)
-            .find(".delete-button")
-            .on("click", function (event) {
-                let id = event.target.attributes.value.value;
-                let name = $(event.target)
-                    .closest("tr")
-                    .find("td > div > span")
-                    .text();
-                let parent = $(event.target).closest("tr");
-                deleteTerm(id, name, parent);
-            });
-        $(term)
             .find(".edit-button")
             .on("click", function () {
                 $($(this)).css("display", "none");
@@ -124,29 +113,3 @@ $(function () {
         });*/
     });
 });
-
-function deleteTerm(id, name, parent) {
-    // Reset Modal
-    $("#confirm-delete button.delete-button").attr("disabled", true);
-    $("#confirm-delete input").val("");
-    $("#confirm-delete p  b").text(name);
-    $("#confirm-delete").toggleClass("hidden"); // Activate modal
-    // Compare when changing input
-    $("#confirm-delete input").on("input", function () {
-        $(this).val() == name
-            ? $("#confirm-delete button.delete-button").attr("disabled", false)
-            : $("#confirm-delete button.delete-button").attr("disabled", true);
-    });
-    $("#confirm-delete button.delete-button").one("click", function () {
-        $.ajax({
-            headers: {
-                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
-            },
-            url: `/cursos/${id}`,
-            method: "DELETE",
-        }).done(() => {
-            $(parent).fadeOut();
-            $("#confirm-delete").toggleClass("hidden");
-        });
-    });
-}
