b50d23fb4c15376c56ad720ca7dad439acbb00b8
aleexnl <alex.nieto0027@gmail.com>
2021-03-15 15:50:09 +0100

    add: course list

---
+++ resources/views/terms/show.blade.php
@@ -0,0 +1,25 @@
+<x-admin-layout>
+    <x-slot name="header">
+        Cursos
+    </x-slot>
+    <table class="w-full terms">
+        <thead>
+            <tr>
+                <th>Codi</th>
+                <th>Nom</th>
+                <th>Descripció</th>
+            </tr>
+        </thead>
+        <tbody>
+            @if ($careers)
+            @foreach ($careers as $career)
+            <tr>
+                <td>{{$career->code}}</td>
+                <td>{{$career->name}}</td>
+                <td>{{$career->description}}</td>
+            </tr>
+            @endforeach
+            @endif
+        </tbody>
+    </table>
+</x-admin-layout>
